7f757323d9c66ea3a34fbb7651f35445b2e60db7
Mary McGrath <m.c.mcgrath13@gmail.com>
2022-12-01 11:06:14 -0500

    feat: day 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2022
+
+Solutions to advent of code puzzles - languages vary by day.
---
+++ day01/src.jl
@@ -0,0 +1,44 @@
+function part_1()
+    max_elf = 0
+    cur_elf = 0
+    for line in eachline(joinpath(@__DIR__, "input1.txt"))
+        if line == ""
+            if cur_elf > max_elf
+                max_elf = cur_elf
+            end
+            cur_elf = 0
+        else
+            cals = parse(Int, line)
+            cur_elf += cals
+        end
+    end
+
+    return max_elf
+end
+
+function part_2()
+    max_elfs = [0, 0, 0]
+    cur_elf = 0
+    for line in eachline(joinpath(@__DIR__, "input1.txt"))
+        if line == ""
+            i = searchsortedfirst(max_elfs, cur_elf; rev=true)
+            if i <= 3
+                update_sorted_list!(max_elfs, i, cur_elf)
+            end
+            cur_elf = 0
+        else
+            cals = parse(Int, line)
+            cur_elf += cals
+        end
+    end
+
+    return sum(max_elfs)
+end
+
+function update_sorted_list!(arr, i, value)
+    # shuffle inds up til length
+    for j in (length(arr) - 1):-1:(i)
+        arr[j+1] = arr[j]
+    end
+    arr[i] = value
+end
